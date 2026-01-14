7c668ccb9b71f6ca19295cee3f07e433eaabf9da
flavdotexe <flavxfer@pm.me>
2026-01-13 21:14:38 -0300

    Primeiro commit: adicionando script e README

---
+++ update_arch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOGFILE="$HOME/.system_update.log"
+
+echo -e "\n Arch maintenance utility"
+echo "----------------------------------"
+
+read -p "Create snapshot before update? (Y/n): " SNAP
+if [[ "$SNAP" == "y" || "$SNAP" == "Y" ]]; then
+    echo "Creating snapshot..."
+    sudo snapper create --description "snapshot-manual-$(date +%F_%H-%M)"
+    echo "Snapshot created successfully!"
+else
+    echo "Skipping snapshot..."
+fi
+
+
+echo
+echo "Starting update system..."
+
+if command -v paru &>/dev/null; then
+    paru -Syu --noconfirm
+else
+    sudo pacman -Syu --noconfirm
+fi
+
+echo
+echo "Cleaning cache.."
+
+sudo paccache -r
+sudo pacman -Rns $(pacman -Qtdq 2>/dev/null) --noconfirm
+echo "Clean system!"
+
+echo
+read -p "Reboot system now? (Y/n): " REBOOT
+if [[ "$REBOOT" == "y" || "$REBOOT" == "Y" ]]; then
+    echo "Salving log..."
+    echo -e "[$(date '+%d-%m-%Y %H:%M')] System updated and successfully cleaned.\n" >> "$LOGFILE"
+    sudo reboot
+    fi
